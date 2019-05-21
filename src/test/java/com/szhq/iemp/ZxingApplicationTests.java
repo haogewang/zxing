0e05d5ba4f00dfd30d6d6f467c63d54c0ac98c1c
wanghao <854805698@qq.com>
2019-05-21 10:41:46 +0800

    rgr

---
+++ src/test/java/com/szhq/iemp/ZxingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szhq.iemp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZxingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
